46087843e83107d08c006065b80865c17d1dd8b0
Jayc Yong <yong.chuin@factset.com>
2020-08-28 07:39:24 +0100

    test

---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("fahwuiofgh awfghaowig haopwugh apuwoighaopuwghaouwghaouwghoaw8yh g08awg ow[ahg[0 awhg0[a w08ghy aw0g[8ha ogfh8awoi gha owgh aowgha0w8g hyaw0ogh[ aw0ogh aw[0goahw0go[a8why[g0 hwa0gh aw gawg[ahw fwa");
+        Debug.Log("Test");
+        Debug.Log("Test");
+        Debug.Log("Test");
+        Debug.Log("Test");
+    }
+
+    // TODO
+    // TODO
+    // TODO
+    // TODO
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int Count
+    {
+        get { return count; }
+        set { count = 42; } // Noncompliant
+    }
+}
